2998eaedff0826e05e691816d7ac67d6163f13d0
Your Name <user@example.com>
2026-07-08 13:27:34 +0530

    Restore adapter-pg configuration

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,4 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
+const { PrismaPg } = require('@prisma/adapter-pg');
+const { Pool } = require('pg');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -9,8 +11,15 @@ if (!connectionString) {
   throw new Error('DATABASE_URL is not defined in environment variables');
 }
 
-const prisma = new PrismaClient({
-  datasourceUrl: connectionString,
-});
+const poolConfig = { connectionString };
+
+if (process.env.NODE_ENV === 'production') {
+  poolConfig.ssl = { rejectUnauthorized: false };
+}
+
+const pool = new Pool(poolConfig);
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
 
 module.exports = prisma;
